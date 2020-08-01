e852fdc041feede9917846c0ccb0d533b802835f
brenu <bvsousa64@gmail.com>
2020-08-01 20:39:26 -0300

    :rocket: Implement login route

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,4 +6,10 @@ routes.get("/", (req, res) => {
   return res.status(200).json({ message: "Hello World!" });
 });
 
+routes.get("/login", (req, res) => {
+  return res.redirect(
+    "https://accounts.spotify.com/authorize?client_id=e5e0a3b8cdd6434dadcd232b7e483fbe&response_type=code&redirect_uri=https://spotify-lyrics.netlify.app/&scope=user-read-private%20user-read-email"
+  );
+});
+
 module.exports = routes;
