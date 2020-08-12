e513c987537f90217cbc98a11bd2ce11cb845508
brenu <bvsousa64@gmail.com>
2020-08-11 22:08:10 -0300

    :arrow_up: Connect with the api in my new droplet

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://spotify-lyrics-backend.herokuapp.com/",
+  baseURL: "https://spotify-api.brenu.ga/",
 });
 
 export default api;
